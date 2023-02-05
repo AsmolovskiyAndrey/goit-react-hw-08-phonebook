7c37a13ee0898157b324e2c5178b0d0b13b3eee9
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2023-02-05 19:20:14 +0100

    start_dom

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ import './index.css';
 import { store } from './components/redux/store';
 import { Provider } from 'react-redux';
 
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
